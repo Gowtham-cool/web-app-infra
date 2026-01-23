0a62009fa1c4751444e4ff39d1c09aea02254e80
Gowtham-cool <gowthameee002@gmail.com>
2026-01-23 19:42:10 +0530

    Update security_ec2.tf

--- security_ec2.tf
+++ security_ec2.tf
@@ -1,3 +1,64 @@
+########################################
+# security_ec2.tf
+########################################
+
+# Security Group for EC2 / ALB
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Security group for web server and ALB"
+  vpc_id      = aws_vpc.main.id
+
+  # SSH access (optional, for demo you can allow all)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # WARNING: open to all; restrict in production
+  }
+
+  # HTTP access
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # All outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+
+########################################
+# EC2 instance
+########################################
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0f5ee92e2d63afc18" # Amazon Linux 2
+  instance_type           = "t2.micro"
+  subnet_id               = aws_subnet.public[0].id
+  vpc_security_group_ids  = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install nginx -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "${var.project_name}-ec2"
+  }
+}
 # Security Group for Web Server
 resource "aws_security_group" "web_sg" {
   name        = "${var.project_name}-sg"
