e2cbfe7efec245a5c69c4712f8a6f6f351676b8e
Gowtham-cool <gowthameee002@gmail.com>
2026-01-23 19:32:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
-data "aws_availability_zones" "available" {}
-
+# Create VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-
-  tags = {
-    Name = "${var.project_name}-vpc"
-  }
+  tags = { Name = "${var.project_name}-vpc" }
 }
+
+# Public subnets
+data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "public" {
   count                   = 2
@@ -16,28 +15,23 @@ resource "aws_subnet" "public" {
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index)
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[count.index]
-
-  tags = {
-    Name = "${var.project_name}-public-${count.index}"
-  }
+  tags = { Name = "${var.project_name}-public-${count.index}" }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags   = { Name = "${var.project_name}-igw" }
 }
 
+# Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = { Name = "${var.project_name}-public-rt" }
+  route { cidr_block = "0.0.0.0/0", gateway_id = aws_internet_gateway.igw.id }
+  tags   = { Name = "${var.project_name}-public-rt" }
 }
 
+# Associate subnets
 resource "aws_route_table_association" "public_assoc" {
   count          = 2
   subnet_id      = aws_subnet.public[count.index].id
