f042ecf4368d7c21021eef9e25ceb9c88dde5a41
Gowtham-cool <gowthameee002@gmail.com>
2026-01-23 19:32:55 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "alb_dns" {
-  value = aws_lb.web_alb.dns_name
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
-output "ec2_id" {
-  value = aws_instance.web.id
+output "public_subnets" {
+  value = aws_subnet.public[*].id
 }
