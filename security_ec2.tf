26d26266fd49211f68d7d6115eb44cfc2e445e35
Gowtham-cool <gowthameee002@gmail.com>
2026-01-25 13:57:18 +0530

    Update security_ec2.tf

--- security_ec2.tf
+++ security_ec2.tf
@@ -34,11 +34,25 @@ resource "aws_security_group" "web_sg" {
 }
 
 # -------------------------------
+# EC2 AMI Data Source (dynamic)
+# -------------------------------
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# -------------------------------
 # EC2 Instance
 # -------------------------------
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18" # Amazon Linux 2
+  ami                    = data.aws_ami.amazon_linux_2.id
   instance_type          = "t2.micro"
   subnet_id              = length(var.public_subnet_ids) > 0 ? var.public_subnet_ids[0] : aws_subnet.public[0].id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
