9b7ca4f02ec9552e8450f0d7d2a2b93c825cebc8
Gowtham-cool <gowthameee002@gmail.com>
2026-01-28 16:44:13 +0530

    Update codebuild.tf

--- codebuild.tf
+++ codebuild.tf
@@ -1,6 +1,6 @@
 resource "aws_codebuild_project" "web_build" {
   name          = "${var.project_name}-build"
-  service_role = aws_iam_role.codebuild_role.arn
+  service_role = data.aws_iam_role.codebuild_role.arn
 
   artifacts {
     type = "CODEPIPELINE"
